5d6f673c17fdc2bc66e756eaa904c81d3b271eb1
NwoguGlory1 <glorynwogu18@gmail.com>
2023-04-25 17:07:17 -0700

    A program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include 'main h'
+/**
+ * main- Entyry point
+ * Description: 'prints _putcher to stdout'
+ * Return; always 0
+ */
+int main(void)
+{
+	_putchar(_)
+	_putcher(p);
+	_putcher(u);
+	_putcher(t);
+	_putcher(c);
+	_putcher(h);
+	_putcher(e);
+	_putcher(r);
+	_putcher('\n');
+	return (0);
