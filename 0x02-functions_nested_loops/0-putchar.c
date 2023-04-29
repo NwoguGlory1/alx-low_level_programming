99c0cefb81d9801e8435220aa1119a11ac37ebc1
NwoguGlory1 <glorynwogu18@gmail.com>
2023-04-29 15:43:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include 'main h'
+#include "main h"
 /**
  * main- Entry point
  * Description: 'prints _putcher to stdout'
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar(_);
-	_putcher(p);
-	_putcher(u);
-	_putcher(t);
-	_putcher(c);
-	_putcher(h);
-	_putcher(e);
-	_putcher(r);
-	_putcher('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('e');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include 'main.h'
+/**
+ *  prints 10 times the alphabet, in lowercase.
+ *
+ *  Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int i;
+int j;
+for (j=1; i <=10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n);
+}
+}
