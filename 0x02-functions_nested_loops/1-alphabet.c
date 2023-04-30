90f9a2d724425e8dc008f7e564dfef59d2eafbe0
NwoguGlory1 <glorynwogu18@gmail.com>
2023-04-30 07:59:29 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
- * A function that prints the alphabet, in lowercase.
- *
- * Return: Always 0 (Success)
- */
+*Description: A function that prints the alphabet, in lowercase.
+*
+* Return: Always 0 (Success)
+*/
+
 void print_alphabet(void)
+
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+
+	_putchar(letter);
 
 _putchar('\n');
 }
