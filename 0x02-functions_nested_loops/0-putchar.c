6bda28fdeb444e45ff6ba71a78f4e47a52b0bf71
NwoguGlory1 <glorynwogu18@gmail.com>
2023-04-30 06:50:30 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
-#include "main h"
+#include "main.h"
 /**
- * main- Entry point
- * Description: 'prints _putcher to stdout'
- * Return; always 0
- */
+* main - Entry point
+* Return: always 0
+*/
 int main(void)
 {
 _putchar('_');
@@ -12,8 +11,9 @@ _putchar('u');
 _putchar('t');
 _putchar('c');
 _putchar('h');
-_putchar('e');
+_putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
