ecb5a62249e62ad5b2169b522ac7cedddbabfc2c
NwoguGlory1 <glorynwogu18@gmail.com>
2023-05-05 15:31:08 -0700

    I am  federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,16 +13,17 @@ void jack_bauer(void)
 		for (b = 0; b <= 9; b++)
 			for (c = 0; c <= 5; c++)
 				for (d = 0; d <= 9; d++)
-
-					if (a >= 2 && b >= 4)
+				{
+				if (a >= 2 && b >= 4)
 					{
 						break;
 					}
-	_putchar(a + 48);
-	_putchar(b + 48);
+	_putchar(a + '0');
+	_putchar(b + '0');
 	_putchar(':');
-	_putchar(c + 48);
-	_putchar(d + 48);
+	_putchar(c + '0');
+	_putchar(d + '0');
 	_putchar('\n');
+				}
 }
 
