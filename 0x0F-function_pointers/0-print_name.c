584e8cf9bd4c9541a3b6968f8636e8d05ebc3224
NwoguGlory1 <glorynwogu18@gmail.com>
2023-05-25 06:54:02 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,16 +9,14 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL)
 	{
-		printf("Invalid name\n");
 		return;
 	}
 	else if (f == NULL)
 	{
-		printf("Invalid function pointer\n");
 		return;
 	}
 	else
 	{
-	(f(name));
+		(f(name));
 	}
 }
