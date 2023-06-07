98926b9d1c29a06a94e3588338093d0d4fc893e9
NwoguGlory1 <glorynwogu18@gmail.com>
2023-06-07 09:26:00 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pop_listint- function that deletes the head node of a listint_t linked list
+ * @head:
+ * Return: the head node's data
+ * if the linked list is empty return 0
+ */
+int pop_listint(listint_t **head)
+{
+
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index- function that returns the nth node of a listint_t linked list.
+ * @head:
+ * @index: the index of the node, starting at 0
+ * Return: nth node of a linked list 
+ * or NULL if the node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
