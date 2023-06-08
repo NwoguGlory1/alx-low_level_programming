b0e27e6d59eca5ee111f943d79f011d9aa7a1391
NwoguGlory1 <glorynwogu18@gmail.com>
2023-06-08 08:27:28 -0700

    Free

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,15 +11,14 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *newnode;
-	listint_t *temp = *head;
+
+	listint_t * newnode * temp;
 
 	newnode = malloc(sizeof(listint_t));
 	if (!newnode || !head)
 		return (NULL);
 
 	newnode->n = n;
-	newnode->next = NULL;
 
 	if (idx == 0)
 	{
@@ -28,18 +27,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newnode;
 		return (newnode);
 	}
-}
+
+	temp = *head
 
 	for (i = 0; temp && i < idx; i++)
-	{
-		if (i == idx - 1)
+		temp = temp->next;
+
+		if (!temp || i != idx - 1)
 		{
+			free(newnode);
+			return (0);		
+		}
 			newnode->next = temp->next;
 			temp->next = newnode;
+
 			return (newnode);
-		}
-		else
-			temp = temp->next;
-	}
-	return (NULL);
 }
