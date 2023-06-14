9d2c1d35b32773b439e4fd8030a13dafe4d267f8
NwoguGlory1 <glorynwogu18@gmail.com>
2023-06-14 04:48:36 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file- creates a file
+ * @filename: pointer to name of the file to create
+ * @text_content: pointer to NULL terminated string to write to the file
+ * Return: 1 on success
+ * -1 on failure (file can not be created
+ *  or  file can not be written
+ *  or  write “fails”, etc.)
+ *  -1 filename is NULL
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, len = 0;
+
+	if (filename == 0)
+		return (-1);
+
+	if (!text_content)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(fd, text_content, len);
+
+	if (fd == -1 || x == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
