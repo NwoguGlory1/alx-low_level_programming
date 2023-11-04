8200e03e7d401bf8fa1ffe94d948fe05a415dd7c
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-03 20:38:28 -0700

    I deleted swap files

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: the hash table
+ * Return: the hash table
+ * nothing if ht is NULL.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *hnode;
+	int c = 0;
+	unsigened long int i;
+
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hnode = ht->array[i];
+		while (hnode)
+		{
+			if (c)
+				printf(", ");
+			printf("'%s': '%s'", hnode->key, hnode->value);
+			hnode = hnode->next;
+			c = 1;
+		}
+	}
+	printf("}\n");
