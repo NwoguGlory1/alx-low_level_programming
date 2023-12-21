5b3994b6505e3b134a44dd4bf30b1f014f94985d
NwoguGlory1 <glorynwogu18@gmail.com>
2023-12-20 23:48:09 -0800

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,7 +7,8 @@
     Grid cells are connected horizontally/vertically (not diagonally).
     Grid is rectangular, width and height don’t exceed 100
     Grid is completely surrounded by water, and there is one island (or nothing).
-    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island)."""
+    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+"""
 def island_perimeter(grid):
     """
     calculates the perimeter of the island
