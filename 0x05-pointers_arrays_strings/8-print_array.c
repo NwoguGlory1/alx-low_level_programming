c2d5ddc74ebe63098a815a0555922dc31987e1f4
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-21 15:21:19 -0700

    Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,28 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_array - a function that prints n elements of an array
- * @a: array name
- * @n: is the number of elements of the array to be printed
- * Return: a and n inputs
+ * print_array - prints n elements of an array of integers
+ * @a: an input array
+ * @n: an input integer
+ *
+ * Return: void
  */
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	if (n <= 0)
 	{
-		printf("%d, ", a[i]);
+		printf("\n");
+		return;
 	}
-		if (i == (n - 1))
-		{
-			printf("%d", a[n - 1]);
-		}
-			printf("\n");
-}
 
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%i, ", a[i]);
+		else
+			printf("%i\n", a[i]);
+	}
+}
